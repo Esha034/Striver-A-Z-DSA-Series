b1fdbdcb1b90c7d27f69d142646eb99b21d2c26c
ESHANI BANIK <banikesha03@gmail.com>
2026-07-03 00:12:13 +0530

    Update 5. Rotate Array(LC-189.).py

--- Array/EASY/5. Rotate Array(LC-189.).py
+++ Array/EASY/5. Rotate Array(LC-189.).py
@@ -1,5 +1,9 @@
-#bruteforce code
+1.Bruteforce Approach
 
+#Idea
+a. Rotate the array one step to the right.
+b. Repeat this k times.
+    
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n=len(nums)
@@ -8,9 +12,18 @@ class Solution:
             for j in range(n-1,-1,-1):
                 nums[j]=nums[j-1]
             nums[0]=t
+#Complexity
+Time: O(n × k) 
+Space: O(1)
 
-#better
 
+
+
+2.Better Approach
+#Idea
+Instead of rotating one step at a time,
+directly place every element in its new position.
+    
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n=len(nums)
@@ -20,3 +33,35 @@ class Solution:
            ans[(i+k)%n]=nums[i]
 
         nums[:]=ans
+
+#Complexity
+Time: O(n)
+Space: O(n)
+
+
+
+3.Optimal Approach(Two pointer)
+
+#Idea
+uses the reversal algorithm: 
+       a.reverse the entire array,
+       b.then reverse the first k elements and 
+       c.the remaining n-k elements.
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n=len(nums)
+        k%=n
+        def reverse(left,right):
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+
+#Complexity
+Time: O(n)
+Space: O(1) 
+
